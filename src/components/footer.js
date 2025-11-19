5847aeaf259ca75edc7f7ab3ecab5c7d4a3870b5
Bishnu Neupane <gwbishnu10@gmail.com>
2025-11-19 21:07:22 +0545

    Add ContactForm (Web3Forms) and integrate into footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,7 @@
 
 
+import ContactForm from './ContactForm';
+
 function Footer() { 
     return (
         <footer className="footer" id="contact">
@@ -10,20 +12,7 @@ function Footer() {
             Feel free to reach out for collaborations, projects, opportunities, or just to say hello!
           </p>
 
-          <div className="footer-contact">
-            <p className="footer-contact-item">
-              <input type="text" placeholder="Your Name" className="footer-input" />
-            </p>
-            <p className="footer-contact-item">
-              <input type="email" placeholder="Your Email" className="footer-input" />
-            </p>
-            <p className="footer-contact-item">
-              <textarea placeholder="Your Message" className="footer-textarea"></textarea>
-            </p>
-            <p>
-              <button className="btn footer-btn">Send Message</button>
-            </p>
-          </div>
+          <ContactForm />
 
           <div className="footer-links">
             <a href="https://mail.google.com/mail/?view=cm&fs=1&to=gwbishnu10@gmail.com&su=Hello&body=I%20am%20interested%20in%20your%20work" className="footer-link">( Email )</a>
